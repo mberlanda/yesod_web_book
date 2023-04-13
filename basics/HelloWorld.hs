46bf436726a564629a223e3469f6e2df21f399e8
Mauro Berlanda <mauro.berlanda@gmail.com>
2023-04-13 23:31:37 +0200

    widgets: synopsis

--- basics/HelloWorld.hs
+++ basics/HelloWorld.hs
@@ -11,12 +11,40 @@ mkYesod
   "HelloWorld"
   [parseRoutes|
 / HomeR GET
+/alerts AlertsR GET
 |]
 
 instance Yesod HelloWorld
 
 getHomeR :: Handler Html
 getHomeR = defaultLayout [whamlet|Hello World!|]
+
+getAlertsR :: Handler Html
+getAlertsR = defaultLayout $ do
+  setTitle "My Page Title"
+  toWidget [lucius| h1 { color: green; } |]
+  addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"
+  toWidget
+    [julius|
+            $(function() {
+                $("h1").click(function(){
+                    alert("You clicked on the heading!");
+                });
+            });
+        |]
+  toWidgetHead
+    [hamlet|
+            <meta name=keywords content="some sample keywords">
+        |]
+  toWidget
+    [hamlet|
+            <h1>Here's one way of including content
+        |]
+  [whamlet|<h2>Here's another |]
+  toWidgetBody
+    [julius|
+            alert("This is included in the body itself");
+        |]
 
 main :: IO ()
 main = warp 3000 HelloWorld
